c77fc7018d44a3f3d0c48c6421fe0157ab3cc91c
Prathamesh Dinkar <60666814+sonukiller@users.noreply.github.com>
2022-05-21 00:21:47 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,14 +1,7 @@
-import numpy as np
 import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-
-from sklearn.metrics import classification_report, confusion_matrix
 
 import joblib
 import streamlit as st
